8411a51b1ff1c58ebc5f1471990b8e0f55cf3a78
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-09-21 16:17:29 +0200

    calS tp0 exo3 calculs

--- L2/calculScientifique/TP0/tp0-exo3.c
+++ L2/calculScientifique/TP0/tp0-exo3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-long fact(long num){
-    long res = 1;
+double fact(long num){
+    double res = 1;
     for(int i = 2; i <= num; i++){
         res = res*i;
     }
@@ -12,16 +12,18 @@ long fact(long num){
 
 double sin_approx(double x, int N)
 {
-    /*double res = x;
-    for(int i = 1; i <= N; i++){
-        int sign = pow(-1, i);
-        double num = x*x;
-        num = num*sign;
+    //PETITE
+    // double res = x;
+    // for(int i = 1; i <= N; i++){
+    //     int sign = pow(-1, i);
+    //     double num = x*x;
+    //     num = num*sign;
+    //     double den = ((2*i)+1)*(2*i);
+    //     res += num/den;
+    //     // printf("%lf\n", num/den);
+    // }
 
-        double den = ((2*i)+1)*(2*i);
-        res = res + num/den;
-    }*/
-
+    //TEL QUE
     double res = x;
     for(int i = 1; i <= N; i++){
         double num = pow(x, (double)((2*i)+1));
@@ -35,38 +37,42 @@ double sin_approx(double x, int N)
 
 int main()
 {
-    const int N = 100;
+    const int N = 8;
     float x;
     printf("Donne x: ");
     scanf("%f", &x);
     printf("Sin = %lf\n",sin(x));
     printf("Approximation sin = %lf\n",sin_approx(x,N));
     printf("Difference = %lf\n", sin(x) - sin_approx(x,N));
-    //printf("Difference = %lf\n", sin(x) - sin_approx(x,1));
-    //printf("Difference = %lf\n", sin(x) - sin_approx(x,2));
-    //printf("Difference = %lf\n", sin(x) - sin_approx(x,4));
-    //printf("Difference = %lf\n", sin(x) - sin_approx(x,8));
+    
+    // printf("Difference = %lf\n", sin(x) - sin_approx(x,1));
+    // printf("Difference = %lf\n", sin(x) - sin_approx(x,2));
+    // printf("Difference = %lf\n", sin(x) - sin_approx(x,4));
+    // printf("Difference = %lf\n", sin(x) - sin_approx(x,8));
 
     /*****************
-     * RESULTATS AVEC LES VALEURS ()CHANGER
+     * RESULTATS AVEC LES VALEURS A 6 DECIMALS
+     * Le rang de cette approximation est -2PI <= x <= 2PI
+     * Apres de cette rang, les valeurs sont bon, mais il faut que
+     * sont converti
      * N = 1, x = 0, d = 0 
      * N = 2, x = 0, d = 0
      * N = 4, x = 0, d = 0
      * N = 8, x = 0, d = 0
      *  
-     * N = 1, x = PI, d = 2.026141
-     * N = 2, x = PI, d = 3.02010
-     * N = 4, x = PI, d = 2.632248
-     * N = 8, x = PI, d = 2.516027
+     * N = 1, x = PI (3.14159), d = 2.026113
+     * N = 2, x = PI (3.14159), d = -0.524041
+     * N = 4, x = PI (3.14159), d = -0.006925
+     * N = 8, x = PI (3.14159), d = -0.000000
      * 
-     * N = 1, x = PI/6, d = 0.000326
-     * N = 2, x = PI/6, d = -0.006524
-     * N = 4, x = PI/6, d = -0.005133
-     * N = 8, x = PI/6, d = -0.004609
+     * N = 1, x = PI/6 (0.52359), d = 0.000326
+     * N = 2, x = PI/6 (0.52359), d = -0.000002
+     * N = 4, x = PI/6 (0.52359), d = -0.000000
+     * N = 8, x = PI/6 (0.52359), d = -0.000000
      * 
-     * N = 1, x = 10*PI, d = 5136.296579
-     * N = 2, x = 10*PI, d = 258602.364237
-     * N = 4, x = 10*PI, d = -85574434.467789
-     * N = 8, x = 10*PI, d = -85597422066.085764
+     * N = 1, x = 10PI (31.41592), d = 5136.293755
+     * N = 2, x = 10PI (31.41592), d = -249879.855413
+     * N = 4, x = 10PI (31.41592), d = -76402981.808341
+     * N = 8, x = 10PI (31.41592), d = -61607359552.289543
      ****************/
 }
