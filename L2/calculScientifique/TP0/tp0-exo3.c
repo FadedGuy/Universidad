aa61408d7a63949ca4bbd0ab4b17d51d727d6110
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-09-21 13:40:47 +0200

    calS tp0 exo3 finish long, chica aproximacion entre 0 y 2PI

--- L2/calculScientifique/TP0/tp0-exo3.c
+++ L2/calculScientifique/TP0/tp0-exo3.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
 #include <math.h>
 
+long fact(long num){
+    long res = 1;
+    for(int i = 2; i <= num; i++){
+        res = res*i;
+    }
+    // printf("%d, %d\n", num, res);
+    return res;
+}
+
 double sin_approx(double x, int N)
 {
+    /*double res = x;
+    for(int i = 1; i <= N; i++){
+        int sign = pow(-1, i);
+        double num = x*x;
+        num = num*sign;
+
+        double den = ((2*i)+1)*(2*i);
+        res = res + num/den;
+    }*/
+
     double res = x;
     for(int i = 1; i <= N; i++){
-        double num = x*x, den = (((2*i)+1) * (2*i));
-        if(i % 2 == 0){
-            res += ((num/den)*res);
-        }else{
-            res -= ((num/den)*res);
-        }
+        double num = pow(x, (double)((2*i)+1));
+        double den = fact((2*i)+1);
+        long sign = pow((long)-1, i);
+        res = res + (num*sign)/(den);
     }
+
     return res;
 }
 
 int main()
 {
-    const int N = 200;
+    const int N = 100;
     float x;
     printf("Donne x: ");
     scanf("%f", &x);
+    printf("Sin = %lf\n",sin(x));
+    printf("Approximation sin = %lf\n",sin_approx(x,N));
     printf("Difference = %lf\n", sin(x) - sin_approx(x,N));
-
     //printf("Difference = %lf\n", sin(x) - sin_approx(x,1));
     //printf("Difference = %lf\n", sin(x) - sin_approx(x,2));
     //printf("Difference = %lf\n", sin(x) - sin_approx(x,4));
     //printf("Difference = %lf\n", sin(x) - sin_approx(x,8));
 
     /*****************
-     * RESULTATS AVEC LES VALEURS
+     * RESULTATS AVEC LES VALEURS ()CHANGER
      * N = 1, x = 0, d = 0 
      * N = 2, x = 0, d = 0
      * N = 4, x = 0, d = 0
