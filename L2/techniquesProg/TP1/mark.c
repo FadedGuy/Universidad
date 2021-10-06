5da91308e5b2c90dad4b104e85907b0c3757904d
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-10-06 11:38:15 +0200

    techniquesProg tp1 exo 5 complete

--- L2/techniquesProg/TP1/mark.c
+++ L2/techniquesProg/TP1/mark.c
@@ -3,49 +3,43 @@
 int main()
 {
     int note = -1;
-        char un, deux;
-        scanf("%c", un);        
-        scanf("%c", deux);
+    char str[2] = "aa";
 
-        printf("%c %c\n", un, deux);
     while(!(note >= 0 && note <= 20))
     {
-        /*
-            Usar note y ch como vienen y restarles el valor despues para poder
-            hacer una comparacion directa y no tener que hacer las conversiones 
-            scanf dos chars, puede llegar a dar error, pero es una opcion
-        */
-        /*
-        int ch;
         printf("mark: ");
-        note = getchar() - '0';
-        ch = getchar() - '0';
-        if(note >= 0 && note <= 9)
+        scanf("%2s", str);      
+        //printf("%c %c\n", str[0], str[1]);
+        
+        if(str[0] >= '0' && str[0] <= '9')
         {
-            Deux digits 
-            if(ch >= 0 && ch <= 9) 
+            if(str[1] >= '0' && str[1] <= '9' && (str[0] == '1' || str[0] == '2')) 
             {
-                note = (note * 10) + ch;
+                note = ((str[0] - '0')*10) + (str[1] - '0');        
             }
-        }    
-        printf("%d\n", note);
-        if(note >= 0 && note < 10)
-        {
-            printf("failing\n");
+            else
+            {
+                note = str[0] - '0';
+            }
         }
-        else if(note >= 10 && note < 12)
-        {
-            printf("satisfactory\n");
-        }
-        else if(note >= 12 && note < 16)
-        {
-            printf("good\n");
-        }
-        else if(note >= 16 && note <= 20)
-        {
-            printf("excellent\n");
-        }
-        */
+        //printf("%d\n", note);
+    }
+
+    if(note >= 0 && note < 10)
+    {
+        printf("failing\n");
+    }
+    else if(note >= 10 && note < 12)
+    {
+        printf("satisfactory\n");
+    }
+    else if(note >= 12 && note < 16)
+    {
+        printf("good\n");
+    }
+    else if(note >= 16 && note <= 20)
+    {
+        printf("excellent\n");
     }
 
     return 0; 
