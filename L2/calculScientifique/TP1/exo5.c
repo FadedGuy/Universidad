bd79f6eac3df204dfe54450908621e8fdb8032b3
Kdas <kasiordia@scinfe057.univ-pau.fr>
2021-10-14 09:24:59 +0200

    calculS tp1 morceaux degre 1

--- L2/calculScientifique/TP1/exo5.c
+++ L2/calculScientifique/TP1/exo5.c
@@ -6,7 +6,7 @@ int main()
     int nbElements;
     FILE *pFile;
 
-    pFile = fopen("exo4.dat", "r+");
+    pFile = fopen("exo3.dat", "r+");
     fscanf(pFile, "%d", &nbElements);
 
     double X[nbElements];
@@ -18,17 +18,17 @@ int main()
         fscanf(pFile, "%lg", &Y[i]);
 
     }
-    // printf("%lg\n", par_morceaux(2016.5, X, Y, nbElements));
+    printf("%lg\n", par_morceaux(1998, X, Y, nbElements));
 
 
     // double N[nbElements];
     // differences_divises(N, X, Y, nbElements);
     // printf("Exo4 %lg \tExo5 %lg\n", eval_p_dd(2016.5, N, X, nbElements), par_morceaux(2016.5, X, Y, nbElements));
 
-    for(int k = 0; k <= 1000; k++)
-    {
-        double zk = 2007 + ((10*(double)k) / 1000);
-        printf("%lg %lg\n", zk, par_morceaux(zk, X, Y, nbElements));
-    }
+    // for(int k = 0; k <= 1000; k++)
+    // {
+    //     double zk = 2007 + ((10*(double)k) / 1000);
+    //     printf("%lg %lg\n", zk, par_morceaux(zk, X, Y, nbElements));
+    // }
     return 0;
 }
