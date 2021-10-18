aacbbbbf139144c0d08a78a64077557eca4ff65f
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-10-18 22:41:14 +0200

    techniquesProg tp2 exo4 book_add

--- L2/techniquesProg/TP2/library4.c
+++ L2/techniquesProg/TP2/library4.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
     char *name;
-    char *authors;
+    char **authors;
     int nAuthors;
     char *publisher;
     int year;
-    long isbn;    
+    double isbn;    
 } book_t;
+
+char *days_str[] = {"monday", "tuesday", "wednesday", "thursday", "saturday", "sunday"};
 
 typedef enum{
     monday, tuesday, wednesday, thursday, friday, saturday, sunday
@@ -58,159 +61,70 @@ library_t* library_create()
 
 void book_free(book_t *book)
 {
+    int i;
+    free(book->name);
+    for(i = 0; i < book->nAuthors; i++)
+    {
+        free(book->authors[i]);
+    }
+    free(book->authors);
+    free(book->publisher);
     free(book);
 }
 
 void library_free(library_t *library)
 {
+    int i;
+    free(library->name);
+    free(library->days);
+    for(i=0; i < library->nBooks; i++)
+    {
+        free(library->books);
+    }
     free(library);
 }
 
 int book_add_author(book_t *book, char *author)
 {
-    /*
-        realloc check
-        al meter un autor tenemos que meterlo a la matriz de antes, asi que se tiene que meter como matriz, no como 
-        string porque si no, perdemos memoria
-
-        authors deja memoria libre cuanod asigno sin usar strcpy, se tiene que copiar dentro del array de autores, como 
-        mencionado anterior pero no asignando, sino mas bien con strcpyy
-
-        puede ser?
-    */
-    book = realloc(book, sizeof(book) + sizeof(author));
-    if(book == NULL)
-        return -1;
-    
-    book-> authors = author;
-    book-> nAuthors++;
-    return 0;
-}
-
-/*void book_print(book_t book)
-{   
-    int i;
-    printf("(\"%s\" ,(", book.name);
-    for(i = 0; i < book.nAuthors; i++)
-    {   
-        printf("\"%s\"", book.authors[i]);
-        if(i != book.nAuthors-1)
-        {
-            printf(", ");
-        }
-    }
-    printf("), \"%s\", %d, %ld)", book.publisher, book.year, book.isbn);
-}
-
-void library_print(library_t library)
-{
-    int i;
-    printf("(\"%s\", (", library.name);
-    for(i = 0; i < library.nDays; i++)
-    {   
-        printf("\"");
-        switch(library.days[i])
-        {
-            case 0:
-                printf("monday");
-                break;
-            case 1:
-                printf("tuesday");
-                break;
-            case 2:
-                printf("wednesday");
-                break;
-            case 3:
-                printf("thursday");
-                break;
-            case 4:
-                printf("friday");
-                break;
-            case 5:
-                printf("saturday");
-                break;
-            case 6:
-                printf("sunday");
-                break;
-            default:
-                break;
-        }
-        printf("\"");
-        if(i != library.nDays-1)
-        {
-            printf(", ");
-        }
-    }
-    printf("), (");
-    for(i = 0; i < library.nBooks; i++)
+    /*int i;
+    if(book->nAuthors == 0)
     {
-        book_print(library.books[i]); 
+        book->authors=(char**) malloc(sizeof(char));
+        book->authors[book->nAuthors]=(char*) malloc(strlen(author) * sizeof(char));
+        //strcpy(book->authors[book->nAuthors], author);
+        for(i = 0; i < strlen(author); i++)
+        {
+            book->authors[book->nAuthors][i]=author[i];
+        }
     }
-    printf("))\n");
-}*/
+    else
+    {
+        book->authors=(char**) realloc(book->authors, book->nAuthors+1*sizeof(char));
+        book->authors[book->nAuthors]=(char*) malloc(strlen(author) * sizeof(char));
+        for(i = 0; i < strlen(author); i++)
+        {
+            book->authors[book->nAuthors][i]=author[i];
+        }
+    }
+    book->nAuthors++;
+    return 0;*/
+}
 
 int main()
 {
     book_t *libro = book_create();
     library_t *libreria = library_create();
 
-    if(book_add_author(libro, "Juan Castel") == -1)
-    {
-        printf("Shut");
-    }
-    printf("%s\n", libro->authors);
-    printf("%d\n", libreria->nDays);
+    book_add_author(libro, "Juan");
+
+    //Error with multiple book_add
+    //book_add_author(libro, "Jose Jose");
+    printf("%d %s %s", libro->nAuthors, libro->authors[0], libro->authors[1]);
+
+
 
     book_free(libro);
     library_free(libreria);
-    /*library_t sciencesLibraryt = {"Sciences Library", 
-                                       {monday, tuesday, wednesday, thursday},
-                                        4, 
-                                        {
-                                            {
-                                                "The C Programming Language", 
-                                                {"Brian W. Kernighan", "Dennis M. Ritchie"}, 
-                                                2, 
-                                                "Prentice Hall", 
-                                                1988, 
-                                                9780131103627
-                                            },
-                                            {
-                                                "C: The Complete Reference",
-                                                {"Herbert Schildt"},
-                                                1,
-                                                "McGraw-Hill Education",
-                                                2000,
-                                                9780072121247
-                                            }
-                                        }, 
-                                2};
-
-    library_t novelLibraryt = {"Novel Library", 
-                                    {tuesday, wednesday, thursday, friday},
-                                    4,
-                                    {
-                                        {
-                                            "Harry Potter and the Philospher's Stone",
-                                            {"J. K. Rowling"},
-                                            1,
-                                            "Bloomsbury",
-                                            1977,
-                                            9780747532699
-                                        }, 
-                                        {
-                                            "Harry Potter and the Chamber of Secrets",
-                                            {"J. K. Rowling"},
-                                            1,
-                                            "Bloomsbury",
-                                            1988,
-                                            9780747538493
-                                        }
-                                    },
-                                    2};
-
-    library_print(sciencesLibraryt);
-    library_print(novelLibraryt);*/
 
     return 0;
 }
