7b6200ce5bdccd2feb90eae1b2ef6e323fe17fa0
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-12-26 15:44:54 +0100

    structDon tp4 finish

--- L2/structDon/TP4/arbre.h
+++ L2/structDon/TP4/arbre.h
@@ -34,29 +34,5 @@ FORET planter(FORET f, int i, ARBRE a);
 int nbArbres(FORET f);
 ARBRE iemeArbre(FORET f, int i);
 
-// typedef struct noeud{
-//     element racine;
-//     int nb_sArbres;
-//     struct noeud* sous_arbre[];
-// }*node;
-// typedef node tree;
-
-// typedef struct foret{
-//     tree arbre;
-//     struct foret* suivant;
-// }*forest;
-
-// tree arbreVide();
-// tree construire(element, forest);
-// forest foretVide();
-// forest planter(tree, int, forest);
-
-// BOOL estArbreVide(tree);
-// BOOL estForetVide(forest);
-
-// element racine(tree);
-// forest listeSousArbres(tree);
-// int nombreArbres(forest);
-// tree iemeArbre(forest, int);
 
 #endif
