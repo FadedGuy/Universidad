7b527b6a401bfcc5176ad0639147814a3d689515
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2022-04-14 15:22:17 +0200

    poo projet avance

--- L2/poo/projet/edp.cpp
+++ L2/poo/projet/edp.cpp
@@ -1,20 +1,10 @@
 /*
-    TODO
-        convert param to int in menu() and change argument on functions that use it (need to be compiled/tested)
 
-    QUESTIONS
-        While parse_doc if something fails is the program shut down or just set to default values,
-        ex. strtol in postal-code (Only place it could really fail during parse, since the non-recognized nodes are ignored) 
-        or if not all attributes are found ?
-    
-        In projet file output is given as:
-            ./edp.out: message
-        do we need to put it in aswell since we have it in main in argv[0]
-        or not.
-
-        company.cpp envelope_at() should it verify?
+    Echoue de code
+    Confirmar mensajes de error
+    Convertir parametro a long solo si es necesario, no hacer el calculo antes
+    Confirmar la conversion en CP a int y del param a LONG usando strol y distintos verificaciones
 */
-
 #include <cerrno>
 #include <climits>
 #include <cstdio>
@@ -57,127 +47,13 @@ void handle_v(){
          << "Written by Kevin Aceves <kevin.aceves-siordia@etud.univ-pau.fr> and Baptiste Genthon <baptiste.genthon@etud.univ-pau.fr>.\n";
 }
 
-bool validParamCodePostal(char* p1, int p2, string command){
+bool validParamCodePostal(char* p1, int p2){
     //If conversion was valid
     if(p1 != NULL && p2 != -1){
         return true;
     }
 
-    //If it was not valid because of missing parameter or invalid conversion
-    if(p1 == NULL){
-        cerr << "missing parameter for the " << command << " command\n";
-        return false;    
-    }
-
-    cerr << "invalid parameter for the " << command << " command\n";
     return false;
-}
-
-void menu(company_t company){
-    char str_input[BUFFER];
-    char* command;
-
-    do{
-        char* param;
-        int len_input = 0;
-        int paramInt = 0;
-        int c;
-        
-        cout << "EDP> ";
-        fgets(str_input, BUFFER, stdin);
-        len_input = strlen(str_input)-1;
-
-        // If true clean the buffer, if false erase \n
-        if(str_input[len_input] != '\n'){
-            while((c = getchar ()) != '\n' && c != EOF);
-        }
-        else{
-            str_input[len_input] = '\0';
-        }
-        
-        // If there is an space, it will set command to before the space, and param after the space from str_input
-        command = str_input;
-        param = strstr(str_input, " ");
-        if(param != NULL){
-            param = param + 1;
-            command[len_input-strlen(param)-1] = '\0';
-
-            // Converts param to long in another variable
-            errno = 0;
-            char* temp;
-            paramInt = strtol(param, &temp, 10);
-            
-            if(temp == param || *temp != '\0' || ((paramInt == INT_MIN || paramInt == INT_MAX) && errno == ERANGE)){
-                paramInt = -1;
-            }
-            // Conversion ended with paramLong = param with type long if it worked, or error code 1 if not
-            // We will be using param or paramLong depending whether we need to pass a string or an int
-        }
-        
-        if(len_input > 18){
-            cerr << "too many characters for the command\n";
-        }
-        else if(!strcmp(command, "e")){
-            company.handle_e();
-        }
-        else if(!strcmp(command, "ec")){ 
-            if(validParamCodePostal(param, paramInt, "ec")){
-                company.handle_ec(paramInt);
-            }
-        }
-        else if(!strcmp(command, "ecge")){ 
-            if(validParamCodePostal(param, paramInt, "ecge")){
-                company.handle_ecge(paramInt);
-            }
-        }
-        else if(!strcmp(command, "ecgt")){ 
-            if(validParamCodePostal(param, paramInt, "ecgt")){
-                company.handle_ecgt(paramInt);
-            }
-        }
-        else if(!strcmp(command, "ecle")){ 
-            if(validParamCodePostal(param, paramInt, "ecle")){
-                company.handle_ecle(paramInt);
-            }
-        }
-        else if(!strcmp(command, "eclt")){ 
-            if(validParamCodePostal(param, paramInt, "eclt")){
-                company.handle_eclt(paramInt);
-            }
-        }
-        else if(!strcmp(command, "en")){ 
-            if(param != NULL){
-                company.handle_en(param);
-            }
-            else{
-                cerr << "invalid parameter for the en command\n";
-            }
-        }
-        else if(!strcmp(command, "h")){ 
-            handle_h();
-        }
-        else if(!strcmp(command, "i")){ 
-            company.handle_i();
-            cout << "\n";
-        }
-        else if(!strcmp(command, "n")){ 
-            company.handle_n();
-            cout << "\n";
-        }
-        else if(!strcmp(command, "q")){
-            continue;
-        }
-        else if(!strcmp(command, "v")){ 
-            handle_v();
-        }
-        else if(!strcmp(command, "w")){ 
-            company.handle_w();
-            cout << "\n";
-        }
-        else if(strlen(command) > 0){
-            cerr << "invalid command\n";
-        }
-    }while(strcmp(command, "q"));
 }
 
 // Remember since sender and recipient are the same class we cna just make a function with the same behavior sending either sender or recipient on the if, rather than the same code
@@ -195,9 +71,12 @@ int parse_address(xml_node node, address_t* address){
         else if(!strcmp(i.name(), "postal-code")){
             errno = 0;
             char* temp;
-            long val = strtol(i.first_child().value(), &temp, 10);
-            
-            if(temp == i.first_child().value() || *temp != '\0' || ((val == LONG_MIN || val == LONG_MAX) && errno == ERANGE)){
+            int val = 0;
+                        
+            val = strtol(i.first_child().value(), &temp, 10);
+
+            if(temp == i.first_child().value() || *temp != '\0' || ((val == INT_MIN || val == INT_MAX) && errno == ERANGE)){
+                throw str2i_error(i.first_child().value());
                 return 1;
             }
             address->set_postal_code(val);
@@ -295,16 +174,123 @@ int main(int argc, char** argv){
         cerr << argv[0] << ": unable to parse the document\n";
         return 1;
     }
+
     if(strcmp(doc.first_child().name(), (char*) "company")){ // Check if its a company
         cerr << argv[0] << ": unable to parse the document\n";
         return 1;
     }
-    if(parse_doc(doc.first_child(), &company)){ // Parse to company
-        cerr << argv[0] << ": unable to parse the document\n";
+
+    try{
+        parse_doc(doc.first_child(), &company); // Parse to company
+    }
+    catch(str2i_error &e){
+        cerr << argv[0] << ": an exception occurred (" << e.what() << ")\n";
         return 1;
     }
 
-    menu(company);
+    char str_input[BUFFER];
+    char* command;
+    do{
+        char* param;
+        int len_input = 0;
+        long paramInt = 0;
+        int c;
+        
+        cout << "EDP> ";
+        fgets(str_input, BUFFER, stdin);
+        len_input = strlen(str_input)-1;
+
+        // If true clean the buffer, if false erase \n
+        if(str_input[len_input] != '\n'){
+            while((c = getchar ()) != '\n' && c != EOF);
+        }
+        else{
+            str_input[len_input] = '\0';
+        }
+        
+        // If there is an space, it will set command to before the space, and param after the space from str_input
+        command = str_input;
+        param = strstr(str_input, " ");
+        if(param != NULL){
+            param = param + 1;
+            command[len_input-strlen(param)-1] = '\0';
+
+            // Converts param to long in another variable
+            errno = 0;
+            char* temp;
+            paramInt = strtol(param, &temp, 10);
+            
+            if(temp == param || *temp != '\0' || ((paramInt == LONG_MIN || paramInt == LONG_MAX) && errno == ERANGE)){
+                paramInt = -1;
+            }
+            // Conversion ended with paramLong = param with type long if it worked, or error code 1 if not
+            // We will be using param or paramLong depending whether we need to pass a string or an int
+        }
+        
+        if(len_input > 18){
+            cerr << argv[0] << " too many characters for the command\n";
+        }
+        else if(!strcmp(command, "e")){
+            company.handle_e();
+        }
+        else if(!strcmp(command, "ec")){ 
+            if(validParamCodePostal(param, paramInt)){
+                company.handle_ec(paramInt);
+            }
+        }
+        else if(!strcmp(command, "ecge")){ 
+            if(validParamCodePostal(param, paramInt)){
+                company.handle_ecge(paramInt);
+            }
+        }
+        else if(!strcmp(command, "ecgt")){ 
+            if(validParamCodePostal(param, paramInt)){
+                company.handle_ecgt(paramInt);
+            }
+        }
+        else if(!strcmp(command, "ecle")){ 
+            if(validParamCodePostal(param, paramInt)){
+                company.handle_ecle(paramInt);
+            }
+        }
+        else if(!strcmp(command, "eclt")){ 
+            if(validParamCodePostal(param, paramInt)){
+                company.handle_eclt(paramInt);
+            }
+        }
+        else if(!strcmp(command, "en")){ 
+            if(param != NULL){
+                company.handle_en(param);
+            }
+            else{
+                cerr << argv[0] << " invalid parameter for the en command\n";
+            }
+        }
+        else if(!strcmp(command, "h")){ 
+            handle_h();
+        }
+        else if(!strcmp(command, "i")){ 
+            company.handle_i();
+            cout << "\n";
+        }
+        else if(!strcmp(command, "n")){ 
+            company.handle_n();
+            cout << "\n";
+        }
+        else if(!strcmp(command, "q")){
+            continue;
+        }
+        else if(!strcmp(command, "v")){ 
+            handle_v();
+        }
+        else if(!strcmp(command, "w")){ 
+            company.handle_w();
+            cout << "\n";
+        }
+        else if(strlen(command) > 0){
+            cerr << argv[0] << " invalid command\n";
+        }
+    }while(strcmp(command, "q"));
 
     return 0;
 }
