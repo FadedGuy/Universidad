bf0a0e760b1fd5568da590b473506392a9f35e8b
faded <faded>
2022-09-26 07:57:41 -0500

    TOO TP2 finish

--- L3/TOO/TP2/N_INSEE/src/main/java/com/mycompany/n_insee/Main.java
+++ L3/TOO/TP2/N_INSEE/src/main/java/com/mycompany/n_insee/Main.java
@@ -8,6 +8,8 @@ public class Main {
     //Toujours appelle main. Pour l'usage exterieur
     public static void main(String[] args){
         N_INSEE n_insee = new N_INSEE("1630125388055");
-        System.out.println("Hi");
+        System.out.println("Clef de 1630125388055 est : " + n_insee.clef());
+        N_INSEE n_insee1 = new N_INSEE("3630125388055");
+        System.out.println("Clef de 1630125388055 est : " + n_insee1.clef());
     }
 }
--- L3/TOO/TP2/N_INSEE/src/main/java/com/mycompany/n_insee/N_INSEE.java
+++ L3/TOO/TP2/N_INSEE/src/main/java/com/mycompany/n_insee/N_INSEE.java
@@ -1,14 +1,23 @@
 package com.mycompany.n_insee;
-import java.util.regex;
 /**
  *
  * @author kasiordia
  */
 public class N_INSEE {
-    private final Integer _n_insee;
-    private final Integer _clef;
+    private final long _nInsee; 
     
     N_INSEE(final String n_insee){
-        
+        verification(n_insee);
+        _nInsee = Long.parseLong(n_insee);
+    }
+    
+    public long clef(){
+        return 97 - (_nInsee % 97);
+    }
+    
+    private void verification(String val){
+        if(!java.util.regex.Pattern.matches("^[1-2]\\d{12}", val)){
+            throw new RuntimeException("N INSEE ne respecte pas le format pour un N INSEE");
+        }
     }
 }
