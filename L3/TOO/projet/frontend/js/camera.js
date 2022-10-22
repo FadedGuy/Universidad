8b38456e6309d9878f2bd6c23a144ee30b78e98f
faded <faded>
2022-10-22 10:21:57 +0200

    TOO projet gets code from capture

--- L3/TOO/projet/frontend/js/camera.js
+++ L3/TOO/projet/frontend/js/camera.js
@@ -1,62 +1,97 @@
 "use strict";
-window.addEventListener('DOMContentLoaded', (event) => {
-    Camera.Run();
+let _DOM_ready = null;
+Object.defineProperty(window, "DOM_ready", {
+    value: new Promise(launched_function_when_DOM_ready => {
+        _DOM_ready = launched_function_when_DOM_ready;
+    }), enumerable: false, configurable: false, writable: false
 });
-class Camera {
-    static Run() {
-        // Could maybe combine video and mediaStream into another class or enum
-        const video = window.document.getElementById("camera_feed");
-        video.width = 400;
-        video.height = 400;
-        video.poster = "https://cdn-icons-png.flaticon.com/512/685/685655.png";
-        const canvas = document.createElement('canvas');
-        let mediaStream;
-        let constraints = {};
-        if (navigator.mediaDevices.getSupportedConstraints().hasOwnProperty('facingMode')) {
-            constraints = { audio: false, video: { facingMode: 'user' } };
-        }
-        else {
-            constraints = { audio: false, video: true };
-        }
-        let videoManagement = function () {
-            var _a;
-            if (video.srcObject !== null) {
-                if (mediaStream.getVideoTracks()[0].readyState === "live") {
-                    canvas.setAttribute('width', video.videoWidth.toString());
-                    canvas.setAttribute('height', video.videoHeight.toString());
-                    (_a = canvas.getContext('2d')) === null || _a === void 0 ? void 0 : _a.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-                    let selfie = new Image();
-                    selfie.onload = function () {
-                        mediaStream.getVideoTracks()[0].stop();
-                        console.assert(mediaStream.active === false);
-                        console.assert(mediaStream.getVideoTracks()[0].readyState === "ended");
-                        video.srcObject = null;
-                        video.setAttribute('poster', selfie.src);
-                    };
-                    selfie.src = canvas.toDataURL("image/png");
-                }
-                else {
-                    alert("_my_video.srcObject.getVideoTracks()[0].readyState === \"ended\"");
-                }
-            }
-            else {
-                navigator.mediaDevices.getUserMedia(constraints)
-                    .then(function (stream) {
-                    mediaStream = stream;
-                    video.srcObject = stream;
-                    video.play();
-                })
-                    .catch((error) => {
-                    alert("navigator.mediaDevices.getUserMedia: " + error.message);
+window.document.onreadystatechange = _DOM_ready;
+window.DOM_ready.then(() => {
+    /* Webcam shooting */
+    // Tested with Chrome >= 68, Firefox >= 62, Edge >= 42 and Safari >= 11
+    // WebRTC camera simple test: https://webrtc.github.io/samples/src/content/getusermedia/gum/
+    // HTML tag: '':
+    const _my_video = window.document.getElementById('camera_feed');
+    _my_video.addEventListener('canplay', () => {
+        window.console.log("The video is playing...");
+        window.console.log("_my_video.videoHeight: " + _my_video.videoHeight + " _my_video.videoWidth: " + _my_video.videoWidth);
+    });
+    const _working_canvas = window.document.createElement('canvas');
+    // window.alert("typeof window.navigator.mediaDevices.getUserMedia: " + typeof window.navigator.mediaDevices.getUserMedia); // 'function'
+    // window.alert("Is Promise? " + (window.navigator.mediaDevices.getUserMedia({
+    //     audio: false,
+    //     video: true
+    // }) instanceof Promise)); // 'true'
+    // Safari: see also "Development" tab, option WebRTC
+    /* Not supported by Safari : {video: {width: 512, height: 512, facingMode: 'user'}} */
+    // Looking at device capabilities:
+    // window.alert(JSON.stringify(window.navigator.mediaDevices.getSupportedConstraints()));
+    let constraints = { audio: false, video: true };
+    if (window.navigator.mediaDevices.getSupportedConstraints().hasOwnProperty('facingMode')) {
+        constraints = { audio: false, video: { facingMode: 'user' } }; // Selfie mode...
+    }
+    // For test:
+    // window.navigator.mediaDevices.getUserMedia(constraints).then((stream: MediaStream) => {
+    //     _my_video.srcObject = stream; // Assign the webcam to the "my_video" HTML element...
+    //     _my_video.play(); // Start playing video...
+    // }).catch((error) => {
+    //     window.alert("window.navigator.mediaDevices.getUserMedia: " + error.message);
+    // });
+    const _video_management = () => {
+        if (_my_video.srcObject !== null) {
+            // Video is running, take photography:
+            if (_my_video.srcObject.getVideoTracks()[0].readyState === 'live') {
+                // Before stopping the camera, one records the last frame:
+                _working_canvas.setAttribute('width', _my_video.videoWidth.toString());
+                _working_canvas.setAttribute('height', _my_video.videoHeight.toString());
+                _working_canvas.getContext('2d').drawImage(_my_video, 0, 0, _my_video.videoWidth, _my_video.videoHeight);
+                const _selfie = new Image();
+                _selfie.onload = () => {
+                    // This immediately stops the webcam (Firefox keeps the last frame on screen while Chrome generates a black screen):
+                    _my_video.srcObject.getVideoTracks()[0].stop();
+                    // The stream is no longer active:
+                    window.console.assert(_my_video.srcObject.active === false);
+                    window.console.assert(_my_video.srcObject.getVideoTracks()[0].readyState === 'ended');
+                    // The stream is detached from the '<video>' tag:
+                    _my_video.srcObject = null; // For next shoot...
+                    _my_video.setAttribute('poster', _selfie.src);
+                };
+                _selfie.src = _working_canvas.toDataURL("image/png"); // From canvas to image...
+                Quagga.decodeSingle({
+                    decoder: {
+                        readers: ["ean_reader"] // List of active readers
+                    },
+                    locate: true,
+                    src: _working_canvas.toDataURL("image/png") // or 'data:image/jpg;base64,' + data
+                }, function (result) {
+                    if (result.codeResult) {
+                        const _code_label = window.document.getElementById('code');
+                        _code_label.textContent = result.codeResult.code;
+                        console.log("result", result.codeResult.code);
+                    }
+                    else {
+                        console.log("not detected");
+                    }
                 });
             }
-        };
-        if (window.PointerEvent) {
-            video.onpointerup = videoManagement;
+            else
+                window.alert("_my_video.srcObject.getVideoTracks()[0].readyState === 'ended'");
         }
-        else {
-            video.addEventListener('mouseup', videoManagement, true);
+        else { // New stream is required (with permission?)
+            // Firefox asks for permission again...
+            window.navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+                // Safari issue here:
+                // https://help.milkshake.tv/hc/en-us/articles/115005464689-Videos-not-playing-when-using-Safari-11-web-browser
+                _my_video.srcObject = stream; // Assign the webcam to the "my_video" HTML element
+                _my_video.play(); // Start playing video...
+            }).catch((error) => {
+                window.alert("window.navigator.mediaDevices.getUserMedia: " + error.message);
+            });
         }
-    }
-}
+    };
+    if (window.PointerEvent) // May not work for some browsers...
+        _my_video.onpointerup = _video_management;
+    else // Older Safari versions do not support 'window.PointerEvent': https://caniuse.com/#feat=pointer
+        _my_video.addEventListener('mouseup', _video_management, true);
+});
 //# sourceMappingURL=camera.js.map
