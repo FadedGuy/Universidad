2ccc4a8510ca1aa8cc6d150aee230dc8412da2c5
faded <faded>
2022-12-29 18:33:56 +0100

    visionComputador tp6 finish

--- L3/visionComputador/tp6/cuestiones/5.py
+++ L3/visionComputador/tp6/cuestiones/5.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import numpy as np
 
-video = cv.VideoCapture("imgs/india.mp4")
+video = cv.VideoCapture("imgs/coches.mp4")
 cv.namedWindow("video", cv.WINDOW_NORMAL)
-fps = int(video.get(cv.CAP_PROP_FPS)) - 2
+n_frames = int(video.get(cv.CAP_PROP_FRAME_COUNT))
+cur_frame = 0
 
 prev_frame_gris = None
 prev_pts = None
@@ -18,32 +19,38 @@ while video.isOpened():
     frame_gris = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     if prev_pts is None:
-        prev_pts = cv.goodFeaturesToTrack(frame_gris, 500, 0.05, 10)
-        prev_pts = prev_pts.reshape(-1, 1, 2)
-    
-    pts, status, errors = cv.calcOpticalFlowPyrLK(prev_frame_gris, frame_gris, prev_pts, (15,15), 5, criteria=(cv.TermCriteria_EPS | cv.TermCriteria_MAX_ITER, 10, 10))
-
-    good_pts = pts[status == 1]
-    if trazas is None:
-        trazas = good_pts
+        pts = cv.goodFeaturesToTrack(frame_gris, 500, 0.05, 10)
+        pts = pts.reshape(-1, 1, 2)
     else:
-        trazas = np.vstack((trazas, good_pts))
-    
-    for p in trazas:
-        cv.circle(frame_gris, (p.ravel()), 2, (0,0,0), -1)
+        pts, status, errors = cv.calcOpticalFlowPyrLK(prev_frame_gris, frame_gris, prev_pts, (15,15), 5, criteria=(cv.TermCriteria_EPS | cv.TermCriteria_MAX_ITER, 10, 10))
+
+        good_pts = pts[status == 1]
+        if trazas is None:
+            trazas = good_pts
+        else:
+            trazas = np.vstack((trazas, good_pts))
+        
+        for p in trazas:
+            x, y = p.ravel()
+            cv.circle(frame_gris, (int(x), int(y)), 2, (255,255,255), -1)
+
+
+    prev_pts = pts
+    prev_frame_gris = frame_gris
 
     cv.imshow("video", frame_gris)
 
-    key = cv.waitKey(fps)&0xFF
+    print(f"{cur_frame}/{n_frames}")
+    cur_frame += 1
+
+    key = cv.waitKey()&0xFF
     if key == ord('q'):
         break
-    if key == ord('r'):
+    elif key == ord('r'):
         trazas = None
         prev_pts = None
     
-    prev_frame_gris = frame_gris
-    prev_pts = pts
-
+    
 
 video.release()
 cv.destroyAllWindows()
