c5fd84d1398205d78e7996f0ebd960e4851d6771
Kdas <kasiordia@scinfe140.univ-pau.fr>
2023-04-12 12:10:14 +0200

    RO td1 ...

--- L3/RO/TD1/main2.cpp
+++ L3/RO/TD1/main2.cpp
@@ -1,7 +1,9 @@
 #include <boost/config.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/read_dimacs.hpp>
+#include <boost/property_map/property_map.hpp>
 #include <boost/graph/graph_utility.hpp>
+#include <boost/graph/edmonds_karp_max_flow.hpp>
 #include <boost/graph/graphviz.hpp>
 #include <bits/stdc++.h>
 using namespace boost;
@@ -24,26 +26,126 @@ typedef adjacency_list < listS, vecS, directedS,
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
 typedef boost::graph_traits<Graph>::edge_descriptor edge_t;
 
-set<int> marquer(Graph g, vertex_t s, vertex_t t) {
-    set<int> L;
-    vector<int> marquage(num_vertices(g));
 
-    // Get capacity of a given node
-    L.insert(s);
-    marquage[s] = 1;
-    auto capacity = get(edge_capacity, g);
-
-    while(!L.empty() && marquage[t] == 0) {
-        auto i = L.begin();  
-        L.erase(i);
-        auto neighbors = out_edges(*i, g);
-        // take neighbors and 
-        // if (i->j) and flow(i,j) < capacity(i,j) alors j marque +1 et capacity(j) = min(capacity(i), capacity(j)-flow)  
-        // else if (j->i) and flow(j,i) > 0 (residuel) alors j marque -1 et capacity(j) = min(capacity(i), flow)
-        
-    }
-    return L;
+int min(int a, int b){
+    return a <= b ? a : b;
 }
+
+void marquer(Graph g, vertex_t s, vertex_t t) {
+    // vector<int> alpha(num_vertices(g), 0);
+    // vector<bool> marked(num_vertices(g), false);
+    // vector<int> L;
+    // graph_traits<Graph>::out_edge_iterator e, e_end;
+    // property_map<Graph, edge_capacity_t>::type capacity = get(edge_capacity, g);
+    // property_map<Graph, edge_residual_capacity_t>::type flow = get(edge_residual_capacity, g);
+    // int MAX_FLOW_3 = INT_MIN;
+
+    // alpha[s] = INT_MAX;
+    // alpha[t] = INT_MAX;
+    // marked[s] = true;
+    // L.push_back(s);
+
+    // while(!L.empty() && !marked[t]){
+    // // while(!L.empty()){
+    //     int i = L.front();
+    //     L.erase(L.begin());
+
+    //     for(tie(e, e_end) = out_edges(i, g); e != e_end; e++){
+    //         int j = target(*e, g);
+    //         if(!marked[j] && flow[*e] < capacity[*e]){
+    //             alpha[j] = min(alpha[i], capacity[*e]-flow[*e]);
+    //             cout << *e << " " << flow[*e] << "\n";
+    //             flow[*e] = alpha[j]; // update the flow value for the forward edge
+    //             cout << flow[*e] << "\n";
+    //             // create the reverse edge
+    //             edge_t rev_e = get(edge_reverse, g, *e);
+    //             flow[rev_e] = alpha[j]; 
+    //             marked[j] = true;
+    //             L.push_back(j);
+    //         }
+    //     }
+
+    //     int min_alpha = INT_MAX;
+    //     for(int iter = 0; iter < num_vertices(g); iter++){
+    //         if(alpha[iter] != 0){
+    //             min_alpha = min(min_alpha, alpha[iter]);
+    //         }
+    //     }
+    //     // for(tie(e, e_end) = in_edges(i, g); e != e_end; e++){
+    //     //     int j = target(*e, g);
+    //     //     if(!marked[j] && flow[*e] > 0){
+    //     //         alpha[j] = min(alpha[i], flow[*e]);
+    //     //         marked[j] num_vertices= true;
+    //     //         L.push_back(j);
+    //     //     }
+    //     // }
+    // }
+
+    // if(!marked[t]){
+    //     cout << "pas de chaine augmentante\n";
+    // } else {
+    //     if(marked[3]){
+    //         cout << "chaine augmentante trouve passant par 3 \n";
+    //         cout << alpha[3] << "\n";
+    //     }
+    //     if(marked[5]){
+    //         cout << "chaine augmentante trouve passant par 5 \n";
+    //         cout << alpha[5] << "\n";
+    //     }        
+    // } 
+
+    vector<int> alpha(num_vertices(g), 0);
+    vector<bool> marked(num_vertices(g), false);
+    vector<int> parent(num_vertices(g), -1); // initialize parent vector with -1
+    vector<int> min_flow(num_vertices(g), INT_MAX); // initialize parent vector with -1
+    vector<int> L;
+    graph_traits<Graph>::out_edge_iterator e, e_end;
+    property_map<Graph, edge_capacity_t>::type capacity = get(edge_capacity, g);
+    property_map<Graph, edge_residual_capacity_t>::type flow = get(edge_residual_capacity, g);
+
+    alpha[s] = INT_MAX;
+    alpha[t] = INT_MAX;
+    marked[s] = true;
+    L.push_back(s);
+
+    while(!L.empty() && !marked[t]){
+        int i = L.front();
+        L.erase(L.begin());
+
+        for(tie(e, e_end) = out_edges(i, g); e != e_end; e++){
+            int j = target(*e, g);
+            if(!marked[j] && flow[*e] < capacity[*e]){
+                alpha[j] = min(alpha[i], capacity[*e]-flow[*e]);
+                flow[*e] = alpha[j]; // update the flow value for the forward edge
+                edge_t rev_e = get(edge_reverse, g, *e);
+                flow[rev_e] = alpha[j]; // create the reverse edge
+                marked[j] = true;
+                parent[j] = i; // set the parent of j to i
+                L.push_back(j);
+
+                min_flow[j] = min(min_flow[j], flow[*e]);
+            }
+        }
+    }
+
+    // Now we can trace the augmenting path that passes through node 3 by following the parent vector
+    vector<int> augmenting_path;
+    int current_node = 8;
+    while(parent[current_node] != -1){ // trace the path until we reach the source node (whose parent is -1)
+        augmenting_path.push_back(current_node);
+        current_node = parent[current_node];
+    }
+    augmenting_path.push_back(current_node); // add the source node to the path
+
+    // Print the augmenting path
+    cout << "Augmenting path that passes through node 3: ";
+    for(int i=augmenting_path.size()-1; i>=0; i--){
+        cout << augmenting_path[i] << " " << min_flow[i] << "\n";
+    }
+    cout << endl;
+
+}
+
 
 int main() {
     Graph g;
@@ -56,7 +158,9 @@ int main() {
 
     read_dimacs_max_flow(g, capacity, rev, s, t);
 
-    set<int> res = marquer(g, s, t);
+    // int max_flow = edmonds_karp_max_flow(g, s, t);
+
+    marquer(g, s, t);
     print_graph(g);    
     std::string filenameRes = "res.dot";
     std::ofstream outRes(filenameRes.c_str());
